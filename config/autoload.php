ace1ac530647c2d7826040fc073367ed836a8b52
Jamshid Elmi <dariush1231@gmail.com>
2017-10-19 17:40:45 +0430

    autolaod file

--- config/autoload.php
+++ config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('MY_helper');
+$autoload['helper'] = array('MY_helper','file');
 
 /*
 | -------------------------------------------------------------------
